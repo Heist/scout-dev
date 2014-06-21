9b193801d25be135319a16fcfde3ec348a48f803
pretentiousgit <alex.leitch@gmail.com>
2014-06-21 13:11:31 -0400

    webapp from scratch

--- server.js
+++ server.js
@@ -6,17 +6,15 @@ var port = Number(process.env.PORT || 5000);
 app.configure(function () {
 	app.use(express.logger('\033[90m:date :method :url :response-time\\ms\033[0m \033[31m:referrer \033[0m'));
 	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
 });
 
-//app things go here	
+//app route things go here
 
-
-
-
-
-
-
-
+app.post('/api/greet', function(req, res) {
+	var greet = "hello, "+req.body.name ;
+	res.json({greeting:greet});
+});
 
 
 app.listen(port);
