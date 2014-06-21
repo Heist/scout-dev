53dc097307f52672dc57dc6fb2a3055a4b3dea0f
pretentiousgit <alex.leitch@gmail.com>
2014-06-21 13:41:25 -0400

    Simple routing of overview and add-flow

--- public/js/moderator.js
+++ public/js/moderator.js
@@ -1,2 +1,15 @@
 "use strict";
 // moderator.js
+
+var scoutApp = angular.module('scoutApp',['ngRoute']);
+
+scoutApp.config(function($routeProvider,$locationProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/overview.html'
+		})
+		.when('/add', {
+			templateUrl: 'partials/add.html'
+		})
+});
