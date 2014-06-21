5d3cf1b7ded39b61e85415693910c81ceb59a8e0
pretentiousgit <alex.leitch@gmail.com>
2014-06-21 13:27:06 -0400

    added Scout overview page and angular

--- public/js/moderator.js
+++ public/js/moderator.js
@@ -1,18 +1,2 @@
 "use strict";
 // moderator.js
-
-function modScreenCtrl($scope,$http) {
-
-	$scope.yourName = "nose";
-	$scope.greeting = "";
-
-	$scope.send=function(){
-		var dataOut = {name:$scope.yourName};
-		
-		$http
-			.post('/api/greet',dataOut)
-			.success(function(dataIn){
-				$scope.greeting = dataIn.greeting;
-			});
-	};
-};
