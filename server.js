6140bfcde93541e2aebb1cf4485522e54e49b3f1
pretentiousgit <alex.leitch@gmail.com>
2014-06-21 12:06:56 -0400

    index.html

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var port = Number(process.env.PORT || 5000);
 //express setup
 app.configure(function () {
 	app.use(express.logger('\033[90m:date :method :url :response-time\\ms\033[0m \033[31m:referrer \033[0m'));
-});
-
-app.get('/', function(req, res){
-  res.send('hello world');
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.listen(port);
