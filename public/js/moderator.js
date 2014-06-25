5d6fa3f16cfb9078a95cd7c7eae3b74601dfc486
pretentiousgit <alex.leitch@gmail.com>
2014-06-25 15:15:58 -0400

    app routing is not working appropriately.

--- public/js/moderator.js
+++ public/js/moderator.js
@@ -7,22 +7,23 @@ scoutApp.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
 	$locationProvider
 		.html5Mode(true);
 
-	$urlRouterProvider.otherwise('/home');
+	// $urlRouterProvider.otherwise('/home');
+	// conflicts with routes provided to express
     
-    $stateProvider        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/',
-            templateUrl: 'partials/overview.html'
-        })
+    // $stateProvider        
+    //     // HOME STATES AND NESTED VIEWS ========================================
+    //     .state('home', {
+    //         url: '/',
+    //         templateUrl: 'partials/overview.html'
+    //     })
         
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('add', {
-        	url: '/add',
-            templateUrl: 'partials/add.html'
-            // we'll get to this in a bit       
-        })
-        ;
+    //     // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    //     .state('add', {
+    //     	url: '/add',
+    //         templateUrl: 'partials/add.html'
+    //         // we'll get to this in a bit       
+    //     })
+    //     ;
 
 })
 
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var port = Number(process.env.PORT || 5000);
 	app.use(bodyParser());
 
 	// This route deals enables HTML5Mode by forwarding missing files to the index.html
-	app.all('/*', function(req, res) {
+	//  known to work like this: app.all('/*', function(req, res)
+	app.get('*', function(req, res) {
     	res.sendfile(__dirname + '/public/index.html');
   	});
 
@@ -23,8 +24,8 @@ mongoose.connect('mongodb://localhost/scoutApp');
 var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function callback () {
-  	// yay!
-});
+  		// yay!
+	});
 
 // define model --> later these can be moved to server/models =====
 	var Flow = mongoose.model('Flow', {
@@ -35,21 +36,41 @@ var db = mongoose.connection;
 	});
 
 // ================================================================
-//app routes -- > later these can be moved to server/routes
+//  app routes -- > later these can be moved to server/routes
 // ================================================================
 
-var router = express.Router(); 				// get an instance of the express Router
+var router = express.Router();  // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
 
 	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-	router.get('/', function(req, res) {
-		res.json({ message: 'hooray! welcome to our api!' });	
-	});
+router
+		.get('/', function(req, res) {
+			res.json({ message: 'hooray! welcome to our api!' });	
+		})
+		.post('/', function(req,res){
+			var flow = new Flow();
+			flow.flow_name = req.body.flow_name;
+
+			flow.save(function(err) {
+			if (err)
+				res.send(err);
+			res.json({ message: 'flow added' });
+			})
+		})
+	;
 
 	// more routes for our API will happen here
 
-	// REGISTER OUR ROUTES -------------------------------
-	// all of our routes will be prefixed with /api
-	app.use('/api', router);
+// register routes with app after creation ------------------------
+
+ app.use('/api', router);
+
 
 // Turn on the application ========================================
 
