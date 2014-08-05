72a9a88092ff145db1392945556cc786741c7fa4
pretentiousgit <alex.leitch@gmail.com>
2014-08-05 15:47:56 -0400

    added Tom's Truncate Filter

--- public/js/app.js
+++ public/js/app.js
@@ -62,6 +62,23 @@ scoutApp.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
         };
 }])
 
+.filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length))
+                length = 10;
+
+            if (end === undefined)
+                end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+
+        };
+    });)
 // REPORT CONTROLLER ===========================================================
 .controller('report', ['$scope','$http', '$location', '$stateParams','$state','$sanitize', function($scope, $http, $location,$stateParams,$state, $sanitize){
     // holds the relevant summary
