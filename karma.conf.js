af3fe2e7bdd65642abd5c664e7513d0a638d5641
pretentiousgit <alex.leitch@gmail.com>
2014-09-21 14:14:58 -0400

    implemented jasmine-node and wrote sanitycheck test"

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
       './js/*.js',
       './js/controllers/*.js',
       './js/filters/*.js',
-      '../test/**/*.js'
+      '../test/client/**/*.js'
     ],
 
 
---
+++ test/server/sanitycheck.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// sanitycheck.spec.js
+
+it('works',function(){
+
+});
