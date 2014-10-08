e7717c3fcdafa26bd15c39a4ba2108843a98e197
pretentiousgit <alex.leitch@gmail.com>
2014-10-08 11:31:19 -0400

    added /account route to front end

---
+++ public/js/controllers/account.js
@@ -0,0 +1,8 @@
+// account.js
+'use strict';
+
+// ACCOUNT CONTROLLER ===========================================================
+angular.module('field_guide_controls')
+       .controller('account', ['$scope','$http', '$location', '$stateParams','$rootScope', function($scope, $http, $location, $stateParams, $rootScope){
+		console.log('account touched');
+       }]);
