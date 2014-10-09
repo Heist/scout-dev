9971b29ffe755c6d649ae75ebce65f38758eaff5
pretentiousgit <alex.leitch@gmail.com>
2014-10-09 16:52:48 -0400

    connect to trello is at least opening the correct window.

--- public/js/controllers/account.js
+++ public/js/controllers/account.js
@@ -2,15 +2,16 @@
 'use strict';
 
 // ACCOUNT CONTROLLER ===========================================================
-angular.module('field_guide_controls').controller('account', ['$scope','$http', '$stateParams','$state', '$location', function($scope, $http,$stateParams,$state, $location){
+angular.module('field_guide_controls').controller('account', ['$scope','$http', '$stateParams','$state', '$location', '$window', function($scope, $http,$stateParams,$state, $location, $window){
 	console.log('account touched');
 
 	$scope.connectTrello = function(){
-		$http
-			.get('/connect/trello', {timeout : 5000})
-			.success(function(err,data){
-				console.log(data)
-				// $window.open('/connect/trello');
-			});
+		$window.open('/connect/trello', 'Connect Field Guide to Trello', 'width=450, height=600');
+		// $http
+		// 	.get('/connect/trello', {timeout : 5000})
+		// 	.success(function(err,data){
+		// 		console.log(data)
+		// 		// 
+		// 	});
 	}
 }]);
