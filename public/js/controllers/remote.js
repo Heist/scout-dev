8763c1b2876504936fc3fca24e7aa7273d66587f
pretentiousgit <alex.leitch@gmail.com>
2014-10-21 15:19:32 -0400

    working on getting screencasting jquery in order

---
+++ public/js/controllers/remote.js
@@ -0,0 +1,9 @@
+'use strict';
+// remote.js
+
+// REPORT CONTROLLER ===========================================================
+angular.module('field_guide_controls').controller('remote', ['$scope','$http', '$location', '$stateParams','$state','$sanitize', function($scope, $http, $location,$stateParams,$state, $sanitize){
+
+
+
+}]);
