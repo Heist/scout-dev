fa6672fce2c2581f780dd95eaf7b182a643afbc9
pretentiousgit <alex.leitch@gmail.com>
2014-10-28 13:32:24 -0400

    tasks adding to appropriate new test

--- server/routes/test.js
+++ server/routes/test.js
@@ -81,47 +81,57 @@ app.route('/api/test/:_id')
 
         var promise = Test.findById(req.params._id).populate({path:'_tasks'}).exec();
 
-        promise.then(function(test){
-            console.log('test duped', test);
-            obj = test;
+        promise.then(function(org_test){
+            console.log('test duped', org_test._id);
+            obj = org_test;
 
             var update = {
-                created_by_account : test.created_by_account,
-                created_by_user : test.created_by_user,
-                desc    : test.desc,
-                link    : test.link,
-                name    : test.name,
-                platform: test.platform
+                created_by_account : org_test.created_by_account,
+                created_by_user : org_test.created_by_user,
+                desc    : org_test.desc,
+                link    : org_test.link,
+                name    : org_test.name,
+                platform: org_test.platform
             };
 
-            return Test.create(update, function(err, test){});
-
+            return Test.create(update, function(err, test){
+                if (err){console.log(err);}
+                console.log(test._id);
+            });
         })
         .then(function(new_test){
-            console.log('test duped step 2', new_test);
-                
-            reply.new_test = new_test._id;
+            console.log('test duped step 2', new_test._id, obj._id);
+
+            // var arr = [];
 
             if (obj._tasks){
-                var arr = obj._tasks;
-                
-                var promise = Task.create(arr);
-    
-                promise.then(function(tasks){
-                    async.each(tasks, function(task){
-                        var update = { $push : {_tasks : task._id}};
-                        Test.findOneAndUpdate(new_test._id, update, function(err, test){});
-                    });
-    
-                    return Test.findOne(new_test._id, function(err, test){
-                        console.log('test returned', test);
+                console.log('obj._tasks.length', obj._tasks.length);
+                async.each(obj._tasks, function(task){
+                    // console.log('async task',task);
+                    var make =  {
+                        desc : task.desc,
+                        index : task.index,
+                        name : task.name,
+                        _test : new_test._id
+                    };
+
+                    Task.create(make, function(err, task){
+                        if (err){ console.log(err); }
+                        
+                        var update = { $push : { _tasks : task._id } }; 
+                        console.log('new_test._id', new_test._id);
+                        Test.findOneAndUpdate({'_id' : new_test._id}, update, function(err, test){
+                            console.log(test._id);
+                        });
                     });
                 });
             }
         })
-        .then(function(last_step){
-            console.log('test duped - last step', last_step);
-            res.json(obj);
+        .then(function(){
+            console.log('test duped - last step');
+            Test.findById(reply.new_test).exec(function(err, test){
+                res.json(test);
+            });
         });
     })
     // update one test with new information
