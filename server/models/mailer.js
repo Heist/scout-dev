bdc1c908a93159592986f28bfc59cf5d6af89c1f
pretentiousgit <alex.leitch@gmail.com>
2014-11-04 16:49:35 -0500

    things

--- server/models/mailer.js
+++ server/models/mailer.js
@@ -32,12 +32,17 @@ var Emailer = (function() {
         
         
         var messageData = {
-            to: "<tom@heistmade.com>",
+            to: "<alex.leitch@gmail.com>",
             from: "Field Guide App <contact@fieldguideapp.com>",
             subject: this.envelope_options.subject,
             html: html,
-            generateTextFromHTML: true
-            // ,attachments: attachments
+            generateTextFromHTML: true,
+            attachments: [{
+                filename: 'field_guide_image.jpg',
+                content: 'R0lGODlh8AAiAKIAAP///yMfIH6AgtXX2E1NT+nq677Awp2foiH5BAAAAAAALAAAAADwACIAAAP/CLrc/jDKSau9OOvNOx+HQAQkQQiG5xSGSJbCUah0bd943hjj678EmWog+BkDgoFuyWw6h72jkZCCFAYzyEEqFWSf4LCYafidDIO04RAlCRy8l7dRaCMP6HTIrBz7P1h/TltAVQ8FRXIMZWZfA1FBXw5sQH0bIiaZmpaaDGyaoJyZkgygmUkWiD5Uk5kHC48mbwufQQyYoKgMdaauFYwkrxSPclh1R14sUXMTqiWkF3Y/hj6eXAGcL9AK0i/CEsRGs7SKCsAEt96lXN8ABdfjEQOVF8dSKYlH7RSEAegb3V5Qe2GNSzYS2wAEJGEIwsKG/cadSxdsHRdD77jEc/jC/9KCNTEMkLJnxFwXByAExPCooN8+C8tUytQFoBo5GDOZKdTGMdgBSm7A+cCT71/LcgAmLsjXLspPoP4WZHSTs+EDYC+ByZE09QeKbkYtFmqQL6GEKCwb2DwaNEKUhG8VdDUbcYGPL3VNlqAYoGlHuXcBE8zQI6xYejev/UgLoN/QXeoI/42wtjHSB3Ex8+Q2+YHLpZ3zJgXC1y+Jg9gEk8gwj6EWKR4XirMipV0iwxXQSqgsWjPCngG+5LNasC9o12yRfCR93PgC3QqGqw6QgRBuqVLC5lOcGsJ2H5Zad4/W+QHvyw4yp98MQPrriuZkWhKtNHpkzqebV+maIVE8If/NTbPcUDMQscoiHmnlQzw9vDQBdBCc1xZwcLHnnmf3vaecXlE1Z9p47vGHQQ+GMIKCAuJ5xYAPVuXDoD8AgkXRRhTEJJNVEvoz0z7qNaDehZNkiOGE9bWXIYQhLjiTWQqEtooM3by4oY8TdoUECFLgNSFMtak1WHIqilXhb/YhN6RzEdDHXJlo7pQfm/txwWRNb1o2xRFhNfiAf9jdecR8W+bW5Ypf2jmbmBTql6Y3iFSVmERrGgkffiAKNN0PczrJ3ZYk7lnlpuwRQuODQGiCY6H9gMIje1SSKWkAxCUmgxHfqLmXh2JZkmSpo5AKKyygTknneAVJyZ12gdZYngP/OY76HKuIwrloMFaiaWuHbH6oq6XuVFYBn89uagkw2zCC7KZSOnjWsl6u9ui6rrYanKIavvKTU+9yeG6bSHIr4gXWFXfNudd1W2e1sAFbJ5cLM4sqevKOOa+09eKaHKS3Zpsrvf9aIF5Dsi2sZ0//cWdbxuQ13C51+Sbq28SvxprcN0y1XGTN4VaKXMcMh0XSFB71w1hyQV/TzlTqwkusw+5erLTEX0AY5KQ4G4qxsFW7OR50PP8iJFRbQZbsLoU1EM4qLUIrqMqENm310y7PNe2U+Orb8qvaTpdF12svnVQIeIwk9Zk0prTS1G2m7HfbLCd3gqOU5qSLU/dCvMhQ6C08Nh0Kjp08qdZPQdInVTkNg1gFJCUtluqIF6ws23Y9rFHORlQhm8y008rXFJJkffvoUngtJARnO7vCMnO6U1TywA3dpOwnUVq79D+wDkCKcnz+M4tkHcmO2FxY4BgrVmxn/ANnR5ImJM5TgEv7ndDSyynPzU/+/DRNkP4rIuyDSBv5a4kvltKLAPLCfhdQ0BnSMIA12OF8VnggHhi4h1W0TxAY3EUaUrHBDGLgbMfCHT+ChQTmefCEKCRDyNTnAQMpJoApjKEMn5ASSJwBByxwARBiYMIZ+vCHQAyiEIdIxCIa8Yg4SAAAOw==',
+                encoding: 'base64',
+                cid: 'source@app.fieldguide.com'
+            }]
         };
         var transport = this.getTransport();
         return transport.sendMail(messageData, function(err, message){
