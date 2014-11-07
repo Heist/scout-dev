e5a10fda4acd36ef69d0ca9d1f436388ef7e670a
pretentiousgit <alex.leitch@gmail.com>
2014-11-07 10:41:23 -0500

    hello world socket app up?

--- sockets/socket_server.js
+++ sockets/socket_server.js
@@ -64,14 +64,18 @@ app.get('/', function(req, res) {
 // SOCKET CONFIG ====================================================
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.broadcast.emit('hi');
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+
+
 
 });
 
