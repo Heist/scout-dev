e565b8161599fed91990de1a8152b663db4e07ee
pretentiousgit <alex.leitch@gmail.com>
2014-11-07 10:31:14 -0500

    added chat reception

--- sockets/socket_server.js
+++ sockets/socket_server.js
@@ -63,7 +63,16 @@ app.get('/', function(req, res) {
 
 // SOCKET CONFIG ====================================================
 io.on('connection', function(socket){
-    console.log('a user connected');
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
 });
 
 // io.on('connection', function(client){
