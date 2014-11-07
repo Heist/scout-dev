6dbd659c02fd047dda80815c409eb6885f74c814
pretentiousgit <alex.leitch@gmail.com>
2014-11-07 14:35:08 -0500

    found a good multi-room demo

--- sockets/socket_server.js
+++ sockets/socket_server.js
@@ -7,6 +7,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+var crypto = require('crypto');
 
 
 // http.listen(3000, function(){
@@ -62,97 +63,97 @@ app.get('/', function(req, res) {
 // });
 
 // SOCKET CONFIG ====================================================
-io.on('connection', function(socket){
-  console.log('a user connected');
+// usernames which are currently connected to the chat
+var usernames = {};
 
-  socket.broadcast.emit('hi');
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+// rooms which are currently available in chat
+var rooms = ['room1','room2','room3'];
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.sockets.on('connection', function (socket) {
 
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // store the username in the socket session for this client
+        socket.username = username;
+        // store the room name in the socket session for this client
+        socket.room = 'room1';
+        // add the client's username to the global list
+        usernames[username] = username;
+        // send client to room 1
+        socket.join('room1');
+        // echo to client they've connected
+        socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+        // echo to room 1 that a person has connected to their room
+        socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'room1');
+    });
 
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
 
+    socket.on('switchRoom', function(newroom){
+        // leave the current room (stored in session)
+        socket.leave(socket.room);
+        // join new room, received as function parameter
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+        // sent message to OLD room
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+        // update socket session room title
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
 });
 
-// io.on('connection', function(client){
+// io.on('connection', function(socket){
 //     console.log('connection made');
 
-//     client.emit('news', { hello: 'world' });
+//     socket.emit('news', { hello: 'world' });
 
-//     client.on('my other event', function (data) {
+//     socket.on('my other event', function (data) {
 //         console.log(data);
 //     });
 
-//     client.on('message', function(err, msg){
+//     socket.on('message', function(err, msg){
 //         // this traces the number of rooms open on the server at any given time.
-//         var k = Object.keys(io.sockets.manager.roomClients[client.id]);
+//         var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
 //         if (k[1] !== undefined) {
 //             var chan = k[1].substring(1, k[1].length);
-//             client.broadcast.to(chan).emit('message', err);
+//             socket.broadcast.to(chan).emit('message', err);
 //         }
 //     });
 
 //     // 
-//     client.on('subscribe', function(data) { 
+//     socket.on('subscribe', function(data) { 
 //         var hash = crypto.createHash('md5').update(data.room).digest('hex').substring(0, 8).toLowerCase();
 //         console.log('joining room', hash);
-//         var k = Object.keys(io.sockets.manager.roomClients[client.id]);
-//         client.join(hash);
+//         var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
+//         socket.join(hash);
 //     });
 
 //     // 
-//     client.on('channel', function(data) { 
+//     socket.on('channel', function(data) { 
 //         console.log('joining room', data.room.toLowerCase());
-//         client.join(data.room); 
+//         socket.join(data.room); 
 //     });
 
 //     // TODO: Email screenshots from app to wherever - we do not do this =============
 //     // Also, we are integrated with Mandrill and not Sendgrid, so this will need to be reworked.
-//     client.on('pics', function(data, err){
-
-
-//         if (sendgrid === undefined) {
-//             console.log("received email request but could not service");
-//             return;
-//         }
-
-//         console.log("pics rec'd");
-
-//         var imgs = data.msg.img_array;
-//         var email_addr = decodeURI(data.msg.email);
-//         var today = new Date();
-//         var email = new sendgrid.Email();
-        
-//         email.addTo(email_addr);
-//         email.setFrom("iOStream");
-//         email.setSubject('[iOStream] Screenshots from ' + today.toDateString());
-
-//         email.setText('Hi! Attached are your screenshots with annotations from ' + today.toLocaleString());
-//         email.addHeader({'X-Sent-Using': 'SendGrid-API'});
-//         email.addHeader({'X-Transport': 'web'});
-
-//         for (var ii = 0; ii < imgs.length; ii++)
-//         {
-//             var b64string = decodeURI(imgs[ii]);
-//             b64string = b64string.substring(23);
-
-//             var buf = new Buffer(b64string, 'base64');
-
-//             email.addFile({content: buf, filename: 'ss'+ (ii + 1) +'.jpg'});
-//         }
-
-//         sendgrid.send(email, function(err, json) {  
-//             if (err) { return console.error(err); }
-//             console.log(json);
-//         });
-
-
-//     });
 // });
 
 // ==================================================================
