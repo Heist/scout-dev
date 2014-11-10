963d10dd4b41b4c024cd978470bc6e1b54882245
pretentiousgit <alex.leitch@gmail.com>
2014-11-10 12:32:39 -0500

    added the postMessage ability from Run, took out all ability to control test

--- public/js/controllers/remote.js
+++ public/js/controllers/remote.js
@@ -4,6 +4,8 @@
 // REPORT CONTROLLER ===========================================================
 angular.module('field_guide_controls').controller('remote', ['$scope','$http', '$location', '$stateParams','$state','$sanitize', 'socket', function($scope, $http, $location,$stateParams,$state, $sanitize, socket){
 
+    $scope.messages = [];
+
     var socket = io.connect('http://127.0.0.1:8080/');
     // pure chatroom model, do what one likes with it.
 
@@ -73,17 +75,18 @@ angular.module('field_guide_controls').controller('remote', ['$scope','$http', '
     // Methods published to the scope
     // ==============================
 
-    $scope.changeName = function () {
+    $scope.registerGuest = function (name) {
+        console.log('registered', name);
         socket.emit('change:name', {
-            name: $scope.newName
+            name: name
         }, function (result) {
             if (!result) {
                 console.log('There was an error changing your name');
             } else {
 
-                changeName($scope.name, $scope.newName);
+                changeName(name, $scope.newName);
 
-                $scope.name = $scope.newName;
+                $scope.observer.name = $scope.newName;
                 $scope.newName = '';
             }
         });
@@ -104,4 +107,74 @@ angular.module('field_guide_controls').controller('remote', ['$scope','$http', '
         $scope.message = '';
     };
 
+    // ==============================================================
+    // Methods from Run.js 
+    // ==============================================================
+
+    // set up controller-wide variables
+    $scope.update = {};
+    $scope.update.tests = [];
+    $scope.update.tasks = [];
+
+    $scope.timeline = []; // holds all messages currently in test
+    $scope.glued = true;
+
+    $http
+        .get('/api/run/'+$stateParams._id)
+        .success(function(data){
+            $scope.tests = data;
+            // console.log('how is data built', data);
+
+            // reset variables to clear cache from state changes.
+            $scope.task = {};
+            var message = {};
+
+        });
+
+
+        $scope.postMessage = function(message){
+            // here we create a note object
+            var note = {};
+
+            note.body = message;
+            note.tags = [];
+            note.created = new Date();
+             
+            note._task = $scope.selected._id;
+            note._test = $scope.selected._test;
+            // note._session = $stateParams._id;
+            note._subject = $scope.subject._id;
+
+            $scope.timeline.push(note);
+            // console.log('message pushing to', $scope.selected._id);
+
+            // TODO: this will catch things on both sides of the hash. 
+            // if message has # with no space, post that to message.tags
+
+            var hashCatch = new RegExp(/\S*#\S+/gi);
+            var hashPull = new RegExp(/#/gi);
+            var tagIt = message.match(hashCatch);          
+            
+            if (tagIt){
+                for (var i=0; i < tagIt.length; ++i) {
+                    var msg = tagIt[i].replace(hashPull,'');
+                    // console.log('tag being pushed', msg)
+                    note.tags.push(msg);
+                }
+            }
+            
+            // console.log('note tags', note.tags);
+            var url = '/api/message/';
+            var data_out = note;
+
+            $http.post(url, data_out)
+                .success(function(data){
+                    // console.log('Message pushed: ', data);
+                })
+                .error(function(data){
+                    // console.log('Error: ' + data);
+                })
+
+            $scope.message='';
+        }
 }]);
