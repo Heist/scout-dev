03af5a6d4b8b17d59e6661a0ce2c4bef8423339d
pretentiousgit <alex.leitch@gmail.com>
2014-11-10 12:24:45 -0500

    integrated run screen

--- server/socket_config.js
+++ server/socket_config.js
@@ -60,57 +60,54 @@ module.exports = function (io) {
         console.log('connected a user');
 
         var name = userNames.getGuestName();
+        socket.emit('hello', {greeting: 'hello '+name});
 
-        socket.emit('hello', {greeting: 'hello'});
-
-        // // send the new user their name and a list of users
+        // send the new user their name and a list of users
         socket.emit('init', {
             name: name,
             users: userNames.get()
         });
 
-        // // notify other clients that a new user has joined
-        // socket.broadcast.emit('user:join', {
-        //     name: name
-        // });
+        // notify other clients that a new user has joined
+        socket.broadcast.emit('user:join', {
+            name: name
+        });
 
-        // // broadcast a user's message to other users
-        // socket.on('send:message', function (data) {
-        //     socket.broadcast.emit('send:message', {
-        //         user: name,
-        //         text: data.message
-        //     });
-        // });
+        // broadcast a user's message to other users
+        socket.on('send:message', function (data) {
+            socket.broadcast.emit('send:message', {
+                user: name,
+                text: data.message
+            });
+        });
 
-        // // validate a user's name change, and broadcast it on success
-        // socket.on('change:name', function (data, fn) {
-        //     if (userNames.claim(data.name)) {
-        //         var oldName = name;
-        //         userNames.free(oldName);
+        // validate a user's name change, and broadcast it on success
+        socket.on('change:name', function (data, fn) {
+            if (userNames.claim(data.name)) {
+                var oldName = name;
+                userNames.free(oldName);
 
-        //         name = data.name;
+                name = data.name;
 
-        //         socket.broadcast.emit('change:name', {
-        //             oldName: oldName,
-        //             newName: name
-        //         });
+                socket.broadcast.emit('change:name', {
+                    oldName: oldName,
+                    newName: name
+                });
 
-        //         fn(true);
-        //     } else {
-        //         fn(false);
-        //     }
-        // });
+                fn(true);
+            } else {
+                fn(false);
+            }
+        });
 
         // clean up when a user leaves, and broadcast it to other users
         socket.on('disconnect', function () {
             console.log('goodbye user');
-            // socket.broadcast.emit('user:left', {
-            //     name: name
-            // });
-            // userNames.free(name);
+            socket.broadcast.emit('user:left', {
+                name: name
+            });
+            userNames.free(name);
         });
-
-
     });
 
 // WORKING ROOM DEMO CODE, SERVER-SIDE ==============================
