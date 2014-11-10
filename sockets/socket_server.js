26d3c5b6f27d08a581967bbffe99f4e508b2dab6
pretentiousgit <alex.leitch@gmail.com>
2014-11-10 09:48:05 -0500

    many sockety experiments

--- sockets/socket_server.js
+++ sockets/socket_server.js
@@ -64,97 +64,99 @@ app.get('/', function(req, res) {
 
 // SOCKET CONFIG ====================================================
 // usernames which are currently connected to the chat
-var usernames = {};
+// var usernames = {};
 
-// rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+// // rooms which are currently available in chat
+// var rooms = ['room1','room2','room3'];
 
-io.sockets.on('connection', function (socket) {
+// io.sockets.on('connection', function (socket) {
 
-    // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function(username){
-        // store the username in the socket session for this client
-        socket.username = username;
-        // store the room name in the socket session for this client
-        socket.room = 'room1';
-        // add the client's username to the global list
-        usernames[username] = username;
-        // send client to room 1
-        socket.join('room1');
-        // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-        // echo to room 1 that a person has connected to their room
-        socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-        socket.emit('updaterooms', rooms, 'room1');
-    });
-
-    // when the client emits 'sendchat', this listens and executes
-    socket.on('sendchat', function (data) {
-        // we tell the client to execute 'updatechat' with 2 parameters
-        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-    });
-
-    socket.on('switchRoom', function(newroom){
-        // leave the current room (stored in session)
-        socket.leave(socket.room);
-        // join new room, received as function parameter
-        socket.join(newroom);
-        socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-        // sent message to OLD room
-        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-        // update socket session room title
-        socket.room = newroom;
-        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-        socket.emit('updaterooms', rooms, newroom);
-    });
-
-    // when the user disconnects.. perform this
-    socket.on('disconnect', function(){
-        // remove the username from global usernames list
-        delete usernames[socket.username];
-        // update list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-        // echo globally that this client has left
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-        socket.leave(socket.room);
-    });
-});
-
-// io.on('connection', function(socket){
-//     console.log('connection made');
-
-//     socket.emit('news', { hello: 'world' });
-
-//     socket.on('my other event', function (data) {
-//         console.log(data);
+//     // when the client emits 'adduser', this listens and executes
+//     socket.on('adduser', function(username){
+//         // store the username in the socket session for this client
+//         socket.username = username;
+//         // store the room name in the socket session for this client
+//         socket.room = 'room1';
+//         // add the client's username to the global list
+//         usernames[username] = username;
+//         // send client to room 1
+//         socket.join('room1');
+//         // echo to client they've connected
+//         socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+//         // echo to room 1 that a person has connected to their room
+//         socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+//         socket.emit('updaterooms', rooms, 'room1');
 //     });
 
-//     socket.on('message', function(err, msg){
-//         // this traces the number of rooms open on the server at any given time.
-//         var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
-//         if (k[1] !== undefined) {
-//             var chan = k[1].substring(1, k[1].length);
-//             socket.broadcast.to(chan).emit('message', err);
-//         }
+//     // when the client emits 'sendchat', this listens and executes
+//     socket.on('sendchat', function (data) {
+//         // we tell the client to execute 'updatechat' with 2 parameters
+//         io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 //     });
 
-//     // 
-//     socket.on('subscribe', function(data) { 
-//         var hash = crypto.createHash('md5').update(data.room).digest('hex').substring(0, 8).toLowerCase();
-//         console.log('joining room', hash);
-//         var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
-//         socket.join(hash);
+//     socket.on('switchRoom', function(newroom){
+//         // leave the current room (stored in session)
+//         socket.leave(socket.room);
+//         // join new room, received as function parameter
+//         socket.join(newroom);
+//         socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+//         // sent message to OLD room
+//         socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+//         // update socket session room title
+//         socket.room = newroom;
+//         socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+//         socket.emit('updaterooms', rooms, newroom);
 //     });
 
-//     // 
-//     socket.on('channel', function(data) { 
-//         console.log('joining room', data.room.toLowerCase());
-//         socket.join(data.room); 
+//     // when the user disconnects.. perform this
+//     socket.on('disconnect', function(){
+//         // remove the username from global usernames list
+//         delete usernames[socket.username];
+//         // update list of users in chat, client-side
+//         io.sockets.emit('updateusers', usernames);
+//         // echo globally that this client has left
+//         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+//         socket.leave(socket.room);
 //     });
-
-//     // TODO: Email screenshots from app to wherever - we do not do this =============
-//     // Also, we are integrated with Mandrill and not Sendgrid, so this will need to be reworked.
 // });
+
+io.on('connection', function(socket){
+    console.log('connection made');
+
+    socket.emit('news', { hello: 'world' });
+
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+
+    // A MESSAGE IS WHAT GETS SENT FROM THE APP TO THE FRONT END
+    //  SINCE IT CAN BE LITERALLY ANYTHING, IT IS PROBABLY AN IMAGE
+    socket.on('message', function(err, msg){
+        // this traces the number of rooms open on the server at any given time.
+        var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
+        if (k[1] !== undefined) {
+            var chan = k[1].substring(1, k[1].length);
+            socket.broadcast.to(chan).emit('message', err);
+        }
+    });
+
+    // JOIN A CHANNEL IN PROGRESS
+    socket.on('subscribe', function(data) { 
+        var hash = crypto.createHash('md5').update(data.room).digest('hex').substring(0, 8).toLowerCase();
+        console.log('joining room', hash);
+        var k = Object.keys(io.sockets.manager.roomClients[socket.id]);
+        socket.join(hash);
+    });
+
+    // This handles rooms?
+    socket.on('channel', function(data) { 
+        console.log('joining room', data.room.toLowerCase());
+        socket.join(data.room); 
+    });
+
+    // TODO: Email screenshots from app to wherever - we do not do this =============
+    // Also, we are integrated with Mandrill and not Sendgrid, so this will need to be reworked.
+});
 
 // ==================================================================
 
