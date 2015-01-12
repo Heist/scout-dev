9e6c280543b2671f252e95944d9ba82bc31e8448
pretentiousgit <alex.leitch@gmail.com>
2015-01-12 17:19:51 -0500

    worked on gulp file for build automation

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,39 @@ var rename = require('gulp-rename');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglify');
  
+var src = {
+    images  : 'public/layout/assets/',
+    styles  : 'public/layout/css/',
+    fonts   : 'public/layout/fonts',
+    scripts : 'public/js/'
+};
+
+var dist = {
+    images  : 'dist/public/layout/assets/',
+    styles  : 'dist/public/layout/css/',
+    fonts   : 'dist/public/layout/fonts',
+    scripts : 'dist/public/js/',
+};
+
+
 gulp.task('css', function () {
     return gulp.src([
-        'app/src/css/vendor/normalise.css',
-        'app/src/css/vendor/boilerplate.css',
-        'app/src/css/style.css',
-        'app/src/css/responsive.css'
+        src.styles + 'fieldguide.css'
     ])
     .pipe(concat('style.css'))
-    .pipe(gulp.dest('dist/public/layout/css'));
+    .pipe(gulp.dest(dist.styles));
 });
  
 gulp.task('fonts', function() {
-    return gulp.src('app/src/fonts/*')
-      .pipe(gulp.dest('dist/public/layout/fonts'));
+    return gulp.src(src.fonts+'*')
+      .pipe(gulp.dest(dist.fonts));
 });
  
 gulp.task('images', function() {
-    return gulp.src('app/src/img/*')
-      .pipe(gulp.dest('dist/public/layout/assets'));
+    return gulp.src(dist.images+'*')
+      .pipe(gulp.dest(dist.images));
 });
- 
-gulp.task('watch', function() {
-    gulp.watch('app/src/css/**', ['css']);
-    gulp.watch('app/src/js/**', ['browserify']);
-    gulp.watch('app/src/fonts/**', ['fonts']);
-    gulp.watch('app/src/img/**', ['images']);
-});
- 
+
 gulp.task('build', ['css', 'browserify', 'fonts', 'images']);
  
 gulp.task('default', ['watch']);
