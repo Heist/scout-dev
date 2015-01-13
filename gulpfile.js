b9cb400fc3d673680cd3c7b44cef75910236e041
pretentiousgit <alex.leitch@gmail.com>
2015-01-13 11:18:54 -0500

    separated bower build from app build

--- gulpfile.js
+++ gulpfile.js
@@ -3,60 +3,34 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var filter = require('gulp-filter');
-var usemin = require('gulp-usemin');
-var loader = require('gulp-load-plugins');
 
 var mainBowerFiles = require('main-bower-files');
 var transform = require('vinyl-transform');
 
-var src = {
-    images  : 'public/layout/assets/',
-    styles  : 'public/layout/css/',
-    fonts   : 'public/layout/fonts/',
-    scripts : 'public/js/',
-    bower   : 'bower_components/'
-};
+var bower = mainBowerFiles();
 
-var dist = {
-    images  : 'dist/public/layout/assets/',
-    styles  : 'dist/public/layout/css/',
-    fonts   : 'dist/public/layout/fonts',
-    scripts : 'dist/public/js/',
-};
+var bower = mainBowerFiles({
+        checkExistence: true
+    });
+
+if(!bower.length) {
+    throw new Error('No main files from Bower dependencies found!');
+}
 
 gulp.task('scripts', function () {
-    gulp.src([
-        mainBowerFiles(),
-        src.scripts
-    ])
+    gulp.src( bower )
     .pipe(filter('*.js'))
-    .pipe(uglify())
+    .pipe(uglify({mangle:false}))
+    .pipe(concat('dependencies.js'))
+    .pipe(gulp.dest('dist/public/js'));
+
+    gulp.src('public/js/*')
+    .pipe(filter('*.js'))
+    .pipe(uglify({mangle:false}))
     .pipe(concat('app.js'))
-    .pipe(gulp.dest(dist.scripts));
+    .pipe(gulp.dest('dist/public/js'));
 });
 
-// gulp.task('css', function () {
-//     return gulp.src([
-//         src.styles + 'fieldguide.css'
-//     ])
-//     .pipe(concat('style.css'))
-//     .pipe(gulp.dest(dist.styles));
-// });
- 
-// gulp.task('fonts', function() {
-//     return gulp.src(src.fonts+'*')
-//       .pipe(gulp.dest(dist.fonts));
-// });
- 
-// gulp.task('images', function() {
-//     return gulp.src(dist.images+'*')
-//       .pipe(gulp.dest(dist.images));
-// });
-
-// gulp.task('build', ['css', 'browserify', 'fonts', 'images', 'scripts']);
 gulp.task('default', ['scripts']);
- 
-// gulp.task('default', ['watch']);
