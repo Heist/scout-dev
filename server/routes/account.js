8cd48f43e259a837a0c0df5da737fee22f70d854
pretentiousgit <alex.leitch@gmail.com>
2015-02-13 17:23:46 -0500

    implemented invite route in async much good may it do me.

--- server/routes/account.js
+++ server/routes/account.js
@@ -66,79 +66,81 @@ module.exports = function(app, debug){
     // INVITATION ROUTES ==================================
     app.route('/api/invite/')
         .post(function(req,res){
-
-            var promise = User.findOne({'local.email' : req.body.address }).exec();
-
-            promise.then(function(user){
-                // if there is a user with that name already ....
-                if(user !== null){ 
-                    user._account = req.user._account;
-                    user.save(function(err,data){
-                        console.log('existing user updated', data.user_email);
-
-                        res.json({
-                            email: data.local.email, 
-                            name: data.name, 
-                            _id: data._id, 
-                            _account: data._account,
-                            msg:'user found'
+            async.waterfall([
+                function(callback){
+                    User.findOne({'local.email' : req.body.address })
+                        .exec(function(err, user){
+                            if(!user){
+                                // if there is no user, set user to null.
+                                callback(null, null);
+                            }
+                            else {
+                                // otherwise pass along the user.
+                                callback(null, user.local.email+' already has a Field Guide account.');
+                            }
                         });
-                    });
-                    throw new Error('User found');
-                }
-
-                else {
-                // No user? Find an invitation for that req.body.address
-                    return Invitation.findOne({'user_email' : req.body.address}).exec();
-                }
-            })
-            .then(function(i){
-                // console.log(i);
-                if(i){
-                    res.send('You have already sent that invitation.');
-                } else {
-                    console.log('no invitation exists');
+                },
+                function(user, callback){
                     
+                    if(user !== null ){
+                        // If there is a user, skip the invite chain and return.
+                        callback(null, user);
+                    }
+
+                    Invitation
+                        .findOne({'user_email' : req.body.address})
+                        .exec(function(err, i){
+                            if(!i){ 
+                                // if there is no invitation, pass to next step.
+                                callback(null, null); 
+                            }
+                            else {
+                                // if there is an invite, pass that to results.
+                                callback(null, 'You have already sent that invitation.');
+                            }
+                        });
+                },
+                function(args, callback){
+                    
+                    if(args !== null){
+                        // If either a user or an invitation exists on the system, pass it along.
+                        callback(null, args);
+                    }
+
+                    // Send an invitation in e-mail to whomever.
                     var invite = new Invitation();
-        
                     invite._account = req.user._account;
                     invite.created_by = req.user._id;
                     invite.user_email = req.body.address;
+                    invite.save();
 
-                    invite.save(function(err,data){
-                        if(err) {return res.send (err);}
+                    var envelope_options = {
+                            to: {
+                                email: invite.user_email,
+                            },
+                            author: invite._account,
+                            subject: "Invite from Field Guide",
+                            template: "invite"
+                        };
+
+                    var message_variables = {
+                        created_by: req.user.name,
+                        invite_link: 'http://projects.fieldguideapp.com/login/'+invite._id
+                    };
+
+                    var mailer = new Emailer(envelope_options, message_variables);
+
+                    mailer.send(function(err, result) {
+                        if (err) { 
+                            console.log(err); 
+                        }
+                        callback(null, 'Invitation sent to '+ invite.user_email);
                     });
-
-                    return invite;
                 }
-            })
-            .then(function(invite){
-                // console.log('invite new no id', invite);
-                res.json(invite);
-
-                var envelope_options = {
-                    to: {
-                        email: invite.user_email,
-                    },
-                    author: invite._account,
-                    subject: "Invite from Field Guide",
-                    template: "invite"
-                };
-
-                var message_variables = {
-                    created_by: req.user.name,
-                    invite_link: 'http://'+app.locals.real_url+'/login/'+invite._id
-                };
-
-                var mailer = new Emailer(envelope_options, message_variables);
-
-                mailer.send(function(err, result) {
-                    if (err) { return console.log(err); }
-                    else {
-                        // console.log('Message sent: ' + result.response);
-                    }
-                });
-
+            ], 
+            function(err, results){
+                if(err){console.log(err);}
+                res.send(results);
             });
         });
 
