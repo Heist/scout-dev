4dfd33bda4200a5ad381e7e348dbb35e8403c43d
pretentiousgit <alex.leitch@gmail.com>
2015-02-17 14:14:40 -0500

    folded in stripe public key

--- public/js/controllers/account.js
+++ public/js/controllers/account.js
@@ -41,24 +41,24 @@
                 });
         };
 
-    // STRIPE CHECKOUT ====================================
+  //   // STRIPE CHECKOUT ====================================
 
-		$scope.stripeCheckout = function(){
-			var handler = StripeCheckout.configure({
-			key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-			// image: '/square-image.png',
-			token: function(token) {
-			// Use the token to create the charge with a server-side script.
-			// You can access the token ID with `token.id`
-			}
-			});
+		// $scope.stripeCheckout = function(){
+		// 	var handler = StripeCheckout.configure({
+		// 	key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+		// 	// image: '/square-image.png',
+		// 	token: function(token) {
+		// 	// Use the token to create the charge with a server-side script.
+		// 	// You can access the token ID with `token.id`
+		// 	}
+		// 	});
 
-			handler.open({
-				name: 'Demo Site',
-				description: '2 widgets ($20.00)',
-				amount: 2000
-			});
-		};
+		// 	handler.open({
+		// 		name: 'Demo Site',
+		// 		description: '2 widgets ($20.00)',
+		// 		amount: 2000
+		// 	});
+		// };
     
 
 
