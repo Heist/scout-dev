9102d8d2d32159349a1088491a79fb975b5da2a5
pretentiousgit <alex.leitch@gmail.com>
2015-03-04 17:08:30 -0500

    inserted environment tester, which is bad practice

--- server.js
+++ server.js
@@ -32,37 +32,29 @@ var port = Number(process.env.FIELD_GUIDE_PORT || process.env.PORT || 8080);
 // var port = Number(process.env.FIELD_GUIDE_PORT || 80);
 
 // Test for our ip address and set what environment we're in accordingly
-// if() NODE_ENV=production node app.js
+// if() NODE_ENV= production node app.js
 
-// var os = require('os');
 var ifaces = require('os').networkInterfaces();
 var ipAddress = function(arr){
 	var address;
 	Object.keys(arr).forEach(function (ifname) {
-	  var alias = 0;
-
 	  ifaces[ifname].forEach(function (iface) {
-	    if ('IPv4' !== iface.family || iface.internal !== false) {
-	      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-	      return;
-	    }
-
-	    if (alias >= 1) {
-	      // this single interface has multiple ipv4 addresses
-	      // console.log(ifname + ':' + alias, iface.address);
-	      address = iface.address;
-	    } else {
-	      // this interface has only one ipv4 adress
-	      // console.log(ifname, iface.address);
-	      address = iface.address;
-	    }
+	  	return ('IPv4' !== iface.family || iface.internal !== false) ?
+	  			address :
+	  			address = iface.address;
 	  });
 	});
 	return address;
 };
-console.log(ipAddress(ifaces));
 
+var environment = function(ip){
+	var arr = ip.split(".");
+	return (arr[0].length === 2) ? 'test': 'production';
+};
 
+process.env.NODE_ENV = environment(ipAddress(ifaces));
+
+console.log(process.env.NODE_ENV);
 // GLOBAL VARIABLES =================================================
 var secrets = require(path.join(__dirname,'secrets'));
 app.locals = _.merge(app.locals, secrets);
