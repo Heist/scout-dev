da873622102a33cac7e9cc82430ec488cd1449fd
pretentiousgit <alex.leitch@gmail.com>
2015-03-11 15:09:06 -0400

    made function index

---
+++ server/models/functions/index.js
@@ -0,0 +1,27 @@
+// function index
+// index.js
+'use strict';
+
+module.exports= {
+	addSubject : require('add-subject'),
+	buildObjectList : require('build-object-list'),
+	buildSummary : require('build-summary'),
+	comment : require('comment'),
+	completeTest : require('complete-test'),
+	createInvite : require('create-invite'),
+	defaultTests : require('default-tests'),
+	deleteTest : require('delete-test'),
+	devTests : require('dev-tests'),
+	dupeTests : require('dupe-tests'),
+	editMessage : require('edit-message'),
+	editTest : require('edit-test'),
+	forgotPasswordToken : require('forgot-password-token'),
+	messageFav : require('message-fav'),
+	messageList : require('message-list'),
+	messageNew : require('message-new'),
+	objectUpdate : require('object-update'),
+	resendInvite : require('resend-invite'),
+	resetPassword : require('reset-password'),
+	tagPuller : require('tag-puller'),
+	userCreate : require('user-create')
+}
