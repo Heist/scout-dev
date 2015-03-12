f67bee3f7cc425d83fb47afe117484930d712c84
pretentiousgit <alex.leitch@gmail.com>
2015-03-12 17:14:57 -0400

    first refactoring of new-message creation and effort at tag puller

--- server/models/functions/message-new.js
+++ server/models/functions/message-new.js
@@ -17,14 +17,21 @@ module.exports = function(request, user, next){
 // CREATE A NEW MESSAGE ===================================
 
 // set message variables from request object.
-    var body = request,
-        tags = fn.tagPuller(body) || null,
-        _subject = request._subject,
-        _test = request._test,
-        _task = request._task;
+    var update = {
+        body : request,
+        tags : fn.tagPuller(request) || null,
+        _subject : request._subject,
+        _test : request._test,
+        _task : request._task,
+        user : user
+    };
 
-    var messageMake = function( _subject, _test, body, user){
-        return models.Message.create({ _subject : _subject, _test : _test, body : body, created_by : user });
+    var messageMake = function( make ){
+        return models.Message.create({ 
+            '_subject' : make._subject, 
+            '_test' : make._test, 
+            'body' : make.body, 
+            'created_by' : make.user });
     }
 
     var findMessage = function(_id){ 
@@ -48,110 +55,29 @@ module.exports = function(request, user, next){
 
     };
 
-    function newMessage(){
-        return messageMake(_subject, _test, body, user).then(function(m){
+    function newMessage(make, next){
+        return messageMake(make).then(function(m){
             return findMessage(m._id).then(function(m){
-                async.parallel({
+                return async.parallel({
                     task: function(callback){
-                        modelUpdate('task', _task, m);
+                        modelUpdate('task', make._task, m);
                     },
                     subject: function(callback){
-                        modelUpdate('subject', _subject, m);
+                        modelUpdate('subject', make._subject, m);
                     },
                     tags: function(callback){
-                        async.map(tags, 
+                        async.map(make.tags, 
                             function(tag, callback){
                                 modelUpdate('tag', tag, m);
                             }, callback );
                     }
-                }, 
+                },
                 function(err, results){
-
+                    if(err){console.log(err);}
+                    next(err, results);
+                    console.log({msg: results.msg, tags: results.waterfall.tags});
                 });
-            }
-            }
-        }
+            })
+        })
     }
-
-
-    // async.waterfall([
-    //     function(callback){
-    //         // Create the message
-    //         // models.Message.create(
-    //         //     ,
-    //         //     function(err, msg){
-    //         //         if (err) { console.log(err); } 
-    //         //         callback(null, msg);
-    //         //     });
-    //     },
-    //     function(msg, callback){
-    //         // Return the message with the subject populated
-    //         // models.Message.findById(msg._id)
-    //         //        .populate('_subject')
-    //         //        .exec(function(err, note){
-    //         //             if (err) { console.log(err); }
-    //         //             callback(null, note); 
-    //         //         });
-    //     },
-    //     function(msg, callback){
-    //         // with the message populated, return the new message array
-    //         async.parallel({
-    //             // task: function(callback){
-    //             //     if (_task){
-    //             //         models.Task.findOneAndUpdate(
-    //             //             {'_id':_task},
-    //             //             {$push: { _messages: msg._id }},
-    //             //             {upsert : false },
-    //             //             function(err, task){
-    //             //                 callback(null, task);
-    //             //             });
-    //             //     } else {
-    //             //         callback(null, null);
-    //             //     }
-    //             },
-    //             subject: function(callback){
-    //                 // models.Subject.findOneAndUpdate(
-    //                 //         {'_id': _subject},
-    //                 //         {$push: { _messages: msg._id }},
-    //                 //         {upsert : false },
-    //                 //         function(err, subject){
-    //                 //             callback(null, subject);
-    //                 //         });
-    //             },
-    //             tags: function(callback){
-    //                 // If there are tags, map the messages into them.
-    //                 // reminder: the tags are not attached to the message. The message is attached to tags.
-    //                 if(tags){
-    //                     async.map(tags, 
-    //                         function(tag, callback){
-    //                             // models.Tag.findOneAndUpdate( 
-    //                             //     {name: tag, _test: msg._test}, 
-    //                             //     { $push: { _messages: msg._id },
-    //                             //             name: tag,
-    //                             //             _test: msg._test
-    //                             //     }, 
-    //                             //     {upsert:true}, 
-    //                             //     function(err, data){ 
-    //                             //         callback(null, data);
-    //                             //     });
-    //                         }, 
-    //                         function(err, results){
-    //                             callback(null, results);
-    //                         });
-    //                 } else {
-    //                     callback(null, null);
-    //                 }
-    //             }
-    //         }, function(err, results){
-    //             // End of parallel callback chain
-    //             callback(null, {msg: msg, waterfall: results});
-    //         });
-    //     }
-    // ], 
-    // function(err, results){
-    //     // End of waterfall chain - new message and tags return here.
-    //     console.log('waterfall', results);
-    //     if(err){console.log(err);}
-    //     next(null, {msg: results.msg, tags: results.waterfall.tags});
-    // });
 };
