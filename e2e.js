07fcd2e8209017d073a400118b7b80621bf53699
pretentiousgit <alex.leitch@gmail.com>
2015-03-20 10:43:08 -0400

    protractor successfully installed, run using standalone webdriver process

--- e2e.js
+++ e2e.js
@@ -2,6 +2,12 @@
 // Protractor configuration for end-to-end testing
 
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['/test/public/spec.js']
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: ['test/public/spec.js']
+	// 'browserName': 'phantomjs',
+	// capabilities: {
+	// 'browserName': 'phantomjs',
+	// 'phantomjs.binary.path':'./node_modules/phantomjs/bin/phantomjs',
+	// 'phantomjs.cli.args':['--logfile=phantom.log', '--loglevel=DEBUG']
+	// }
 }
