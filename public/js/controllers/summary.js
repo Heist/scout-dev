e91a81bb08b07da05ae1ea4eead7c6d0a8812327
pretentiousgit <alex.leitch@gmail.com>
2015-04-10 11:32:09 -0400

    nav modifications seem ready.

--- public/js/controllers/summary.js
+++ public/js/controllers/summary.js
@@ -139,7 +139,7 @@
 // the note should disappear from the tag even if it is being edited within that tag
 // it should then appear on any other tag that is on the left nav
 // if it does not have a tag to belong to, it should turn up only on tasks (v 1.0)
-        var navMod = function(data){
+        var addTagsToLeftNav = function(data){
             var navlist_check = _.pluck($scope.navlist, 'name');
             var msg_tag       = _.pluck(data.msg._tags, 'name');
 
@@ -161,6 +161,33 @@
             })
         }
 
+        var pullDeadTags = function(data, message, navlist){
+            var nav_id_list = _.pluck(navlist, '_id');
+            // clear dead entries from the left nav when we edit a message.
+
+            message._tags.map(function(msg_tag, i){
+
+                var new_tag_idx = data.msg._tags.indexOf(msg_tag);
+                var id = (typeof msg_tag === 'object') ? msg_tag._id : msg_tag; // set id to check
+                
+                if(new_tag_idx === -1){                         // that tag no longer exists in that message
+                    var match_in_nav = nav_id_list.indexOf(id); // find the nav entry matching the no-longer-there tag.
+
+                    function strFilter (value){
+                        return value !== message._id;           // filter matching nav entry for old messages
+                    }
+
+                    var match_msg = navlist[match_in_nav]._messages.filter(strFilter);
+
+                    if(match_msg.length === 0){                 // no messages left? Kill the tag and select the next one.
+                        navlist.splice(match_in_nav,1);  // Kill tag in the nav
+                        $scope.activate($scope.navlist[match_in_nav], match_in_nav); // select new one.
+                        
+                    }    
+
+                }
+            })
+        }
 
         $scope.saveEdit = function(message){
             $scope.messageEditToggle = '';
@@ -174,67 +201,8 @@
                     var task_idx = _.pluck($scope.navlist, '_id').indexOf(message._task);
                     $scope.navlist[task_idx]._messages.push(data.msg._id);
 
-                    // edit the messages list of the left navigation.
-                    navMod(data);
-
-                    // map the nav list
-                    // check if the old message had a tag the new message doesn't
-                    // if so, get the index of that tag name in the nav list
-                    // splice it out.
-
-                    // the tag now exists on the nav list
-                    // but it no longer exists in the message
-                    // compare the message to the new message and find which tags are not matched -> -1
-
-                    // if a tag does not exist, check the navlist id list for their id
-                    // if it exists, splice the message from navlist_id_entry
-                    // then check the navlist_id_entry length 
-                    // if the entry length is 0, delete that id from the nav list.
-
-                    var nav_id_list = _.pluck($scope.navlist, '_id');
-                    // var cheat = $scope.navlist.length - 1;
-
-                    // alright, let's try to clear dead entries from the left nav.
-                    message._tags.map(function(msg_tag, i){
-
-                        var new_tag_idx = data.msg._tags.indexOf(msg_tag);
-                        console.log('tag index', new_tag_idx)
-                        // set id to check
-                        var id;
-                        if(typeof msg_tag === 'object'){
-                            id = msg_tag._id;
-                        }
-                        if(typeof msg_tag === 'string'){
-                            id = msg_tag
-                        }
-                        
-                        if(new_tag_idx === -1){
-                            // that tag no longer exists in that message
-                            // we now have a tag id that matches something in the nav.
-                            console.log('this no longer exists on this message', id);
-
-                            // find the nav entry matching the no-longer-there tag.
-                            var match_in_nav = nav_id_list.indexOf(id);
-                            console.log('match in nav - exists?', match_in_nav, id);
-
-                            // in that nav entry, get the messages, and splice out the old message
-                            console.log('does the tag have this message in its list still', message._id, $scope.navlist[match_in_nav]._messages);
-                            function strFilter (value){
-                                return value !== message._id;
-                            }
-                            var match_msg = $scope.navlist[match_in_nav]._messages.filter(strFilter);
-                            console.log('is it dead', match_msg);
-
-                            if(match_msg.length === 0){
-                                console.log('there are no messages left');
-                                $scope.navlist.splice(match_in_nav,1);
-                                
-                                // var pick = $scope.navlist.length;
-                                // $scope.activate($scope.navlist[pick], pick);
-                            }    
-
-                        }
-                    })
+                    addTagsToLeftNav(data); // add new left nav tags to new tags
+                    pullDeadTags(data, message, $scope.navlist); // did we kill a tag? Kill a tag.
                 });
         };
 
@@ -250,7 +218,7 @@
                     $scope.messages[data.msg._subject.name].push(data.msg);
                     $scope.selected._messages.push(data.msg._id);
 
-                    navMod(data);
+                    addTagsToLeftNav(data);
                 });
         };
 
