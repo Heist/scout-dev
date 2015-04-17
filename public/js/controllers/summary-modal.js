779ce0a6d0c5e53f433c1faf7cf31355bc93f065
shrnchn <schan21@live.com>
2015-04-17 11:34:58 -0400

    prepped summary modal scripts and styles

---
+++ public/js/controllers/summary-modal.js
@@ -0,0 +1,47 @@
+// summary-modal.js
+(function() {
+	'use strict';
+
+	angular.module('field_guide_controls')
+	.controller('newTest', 
+    		['testBuildFunctions','$scope','$http','$stateParams','$state','$location','$rootScope',
+    function( testBuildFunctions,  $scope,  $http,  $stateParams,  $state,  $location, $rootScope){
+        console.log('loaded new test');
+    	// SETUP VARIABLES ==========================
+        $scope.test = {};
+
+        $scope.test.created_by_user = $rootScope.user;
+
+    	// TEST UPDATE ==============================
+    	$scope.selectPrototype = function(kind){
+            $scope.test.kind = kind;
+            mixpanel.track('Type of Test', {'test type' : kind });
+        };
+
+        $scope.selectPlatform = function(kind){
+            $scope.test.platform = kind;
+        };
+
+        $scope.updateTest = function(){
+            // reminder: this pushes an update to an already-created test
+        };
+
+        $scope.addTest = function(test){
+        	console.log('new test', test);
+
+            if($scope.test.name){
+                mixpanel.track('Test name changed', { 'user': $rootScope.user });
+            }
+
+            $http
+                .post('/api/test/', test)
+                .success(function(data){
+                    $scope.$parent.tests.push(data);
+                    $scope.$parent.newTestModalToggle();
+                    $location.path('/edit/test/'+ data._id);
+                });
+        }
+
+	}]);
+
+})();
