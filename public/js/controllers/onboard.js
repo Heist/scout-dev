bcdb9009b0025f80cbd1bb787eb012f48be88cd1
shrnchn <schan21@live.com>
2015-04-23 14:17:15 -0400

    added ng class to steps to animate

--- public/js/controllers/onboard.js
+++ public/js/controllers/onboard.js
@@ -27,39 +27,13 @@
 
         console.log('step', $scope.user.onboard);
 
-        var slideInUp = 'animated slideInUp';
-        var slideOutDown = 'animated slideOutDown';
-        var slideOutLeft = 'animated slideOutLeft';
-        var slideInRight = 'animated slideInRight';
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-        $scope.trueFalse = false;
-        var s1 = $element.find('.step-1');
-
-
-        s1.removeClass('hide');
-        
-        s1.addClass('show');
-
-
        // FUNCTIONS =======================================
 
         $scope.changeOnboard = function(num){
             console.log('change on board num', num);
             $scope.user.onboard = num;
-            
             $rootScope.user.onboard = num;
 
-            // if (num === 2) { this.addClass('YOUR CLASS') } // look up how to add class to item in angular
-            // alternately, ng-class? prooooobably not
-          if( num === 2 ){
-            var s2 = $element.find('.step-2');
-            s2.addClass(slideInRight);
-          }
-
-            // $('.modal').addClass(animationUpIn).one(animationEnd, function(){
-            //   $('.step-1').css('display', 'block').addClass(animationUpIn);
-            // });
 
             var url = '/api/user/'+$rootScope.user._id;
             var dataOut = {onboard : $scope.user.onboard};
