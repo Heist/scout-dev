593c5718c9e7f6b176c84050649c20e22666a9a7
pretentiousgit <alex.leitch@gmail.com>
2015-05-04 15:52:40 -0400

    added a change to the email for a reminder message for password resets

--- server/models/functions/forgot-password-token.js
+++ server/models/functions/forgot-password-token.js
@@ -31,7 +31,7 @@ module.exports = function(emailAddress, app, next){
                 subject: 'Field Guide Password Reset',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your Field Guide account.\n\n' +
                 'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                'http://projects.fieldguideapp.com/forgot/' + token + '\n\n' +
+                'http://projects.fieldguideapp.com/reset/' + token + '\n\n' +
                 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
             };
 
