e407fd4c4345e61240e7cbc0d2eefa2abbba9a2a
shrnchn <schan21@live.com>
2015-05-04 14:30:41 -0400

    updated helper text on tag pool section

--- public/js/build.js
+++ public/js/build.js
@@ -51416,10 +51416,13 @@ angular.module("typeahead-popup.html", []).run(["$templateCache", function($temp
                 var dataOut = {password: pass};
                 console.log('touched newPass');
                 $http
-                    .post('/auth/reset/'+$stateParams.token, dataOut)
+                    .post('/auth/reset'+$stateParams.token, dataOut)
                     .success(function(data){
                         // do a login here, perhaps
                         console.log('reset', data);
+                        if(data.length === 0){ 
+                            $scope.successMsg = "I'm sorry, that reset token is broken.";
+                        }
                         if(data.length > 0){
                             $scope.successMsg = data;
                         }
@@ -51843,6 +51846,84 @@ angular.module("typeahead-popup.html", []).run(["$templateCache", function($temp
                 return;
             }
         };
+
+
+    // SOCKET ROUTES - 0.9 ============================================== 
+    // for 1.0 check socket_routes_1.js in /server/
+        
+    // RECIEVE SCREENCAPS FROM THE SOCKET ===============================
+
+        if ($scope.test.kind === 'prototype'){
+            
+            var canvas = document.getElementById('feed'),
+                image = document.getElementById('ia'),
+                context = canvas.getContext('2d');
+
+            $scope.connect = {};
+            $scope.connect.text = '71b';
+
+            socket.on('connect_failed', function(data)
+            {
+                // console.log('connect_failed');
+            });
+
+            socket.on('connecting', function(data)
+            {
+                // console.log('connecting');
+            });
+            socket.on('disconnect', function(data)
+            {
+                // console.log('disconnect');
+
+            });
+
+            socket.on('error', function(reason)
+            {
+                // console.log('error', reason);
+            });
+            socket.on('reconnect_failed', function(data)
+            {
+                // console.log('reconnect_failed');
+            });
+            socket.on('reconnect', function(data)
+            {
+                // console.log('reconnect');
+                // socket.emit('channel', {room : $scope.subject.testroom, test: $stateParams._id});
+            });
+            socket.on('reconnecting', function(data)
+            {
+                // console.log('reconnecting');
+            });
+
+            socket.on('announce', function(data){
+                // console.log('announce', data);
+            });
+
+            socket.on('joined_channel', function(data){ 
+                // console.log('joined_channel', data);
+            });
+
+            socket.on('note', function(data){
+                // console.log('note', data);
+                $scope.timeline.push(data.note.msg);
+                $scope.$apply();
+            });
+
+            socket.on('subject', function(data){
+                socket.emit('join_subject_test', data);
+            });
+
+            socket.on('message',function(data) {
+                // console.log('message');
+                image.src = "data:image/jpg;base64,"+data;
+                canvas.width = 358;
+                canvas.height = 358 * image.height / image.width;
+
+                context.drawImage(image, 0, 0, 358, 358 * image.height / image.width);
+                // context.drawImage(image, 0, 0, 358, 358 * image.height / image.width);
+            });
+        }
+
 
     // ANGULAR ROUTES ===================================================
         $scope.addTask = function(task){
