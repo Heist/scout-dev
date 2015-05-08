2df3fbb325e70fa01f88281dce196103107814cb
pretentiousgit <alex.leitch@gmail.com>
2015-05-08 11:17:39 -0400

    making things more functional

--- server/models/functions/default-tests.js
+++ server/models/functions/default-tests.js
@@ -7,13 +7,13 @@ module.exports = function(account, id, callback){
 // Module dependencies
     var mongoose = require('mongoose');  // can't set an ObjectID without this.
     var _        = require('lodash');
-    var Bluebird = require('bluebird');
-    var models   = Bluebird.promisifyAll(require('../../models'));
+    var Promise = require('bluebird');
+    var models   = Promise.promisifyAll(require('../../models'));
     var fn       = require('../../models/functions');
     var tests    = fn.defaultTestData(account, id);
 
     var modelSave  = function(mongooseModel){
-        return new Bluebird(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             mongooseModel.save(function(err,done) {
               if (!done || done.error) {return reject(done.error);}
               return resolve(done);
@@ -22,14 +22,14 @@ module.exports = function(account, id, callback){
     }
 
     var createTests = function(testObject){
-        return Bluebird.map(tests, function(n){
+        return Promise.map(tests, function(n){
             var test = new models.Test(n.test);
-            return test;
+            return modelSave(test);
         });
     }
 
     var createSubjects = function(test, testSubjects){
-        return Bluebird.map(testSubjects, function(m){
+        return Promise.map(testSubjects, function(m){
             m.name  = m;
             m._test = test._id;
 
@@ -38,27 +38,38 @@ module.exports = function(account, id, callback){
         })
     }
 
-    var createTasks = function(test, testTasks){
-        Bluebird.map(testTasks, function(task, i){
+
+    var createMessages = function(task, taskMessages, subjects){
+        return Promise.map(taskMessages, function(msg, i){
+                var sub = _.filter(subjects, function(s){
+                    return s.name === msg.name;
+                })
+
+                return fn.messageNew({
+                    body     : msg.body,
+                    _subject : sub._id,
+                    _test    : task._test,
+                    _task    : task._id
+                });
+        })
+    }
+
+    var createTasks = function(test, testTasks, subjects){
+        return Promise.map(testTasks, function(task, i){
                         task._test = test._id;
                         task.index = i
 
                         var t = new models.Task(task);
                         test._tasks.push(t._id);
 
-                        // TODO: in here, get the subject id from the subjects array
-                        // _.filter(subjects, function(n){  return n.name === message.name  })
-                        // then create a message-new for every message in the messages array for the task
-                        // then return the saved task in the then step
-                        Bluebird.map(n.tasks._messages, function(note){
-
-                            return modelSave(t);
-                        })
+                        return modelSave(t).then(function(savedTask){
+                            return createMessages(savedTask, task._messages, subjects);
+                        });
                 })
     }
 
     var createTags = function(test, testTags){
-       return Bluebird.map(testTags, function(tag, i){
+       return Promise.map(testTags, function(tag, i){
                 var t = new models.Tag({
                         name      : tag,
                         nameCheck : tag.toLowerCase(),
@@ -70,87 +81,20 @@ module.exports = function(account, id, callback){
             })   
     }
 
-    var createMessages = function(task, taskMessages, subjects){
-        return Bluebird.map(taskMessages, function(msg, i){
-                var sub = _.filter(subjects, function(s){
-                    return s.name === msg.name;
-                })
-
-                var m = fn.messageNew({
-                    body     : msg.body,
-                    _subject : sub._id,
-                    _test    : task._test,
-                    _task    : task._id
-                });
-        })
-    }
-
-// Abstract and create tests 
-
-    // in parallel:
-
-    // Create a test for each
-    // inside the test, create tasks for that test, setting their _test to test._id and index to $index
-    // this works but returns undefin
-
-    Bluebird.map(tests, function(n){
-        var test =  new models.Test(n.test);
-        var subjects;
-        // TODO: in here, create all of the subjects in the test
-        // subjects must be created with a _test 
-        // set them to the variable subject
-        return Bluebird.map(n.subjects, function(n){
-                n.name  = n;
-                n._test = test._id;
-
-                var subj = new models.Subject(n);
-                return modelSave(subj);
-        }).then(function(subjectArray){
-            subjects = subjectArray;
-
-            return Bluebird.all([
-               Bluebird.map(n.tags, function(tag, i){
-                    var t = new models.Tag({
-                            name      : tag,
-                            nameCheck : tag.toLowerCase(),
-                            _test     : test._id
-                        });
-                    test._tags.push(t._id);
-
-                    return modelSave(t);
-                }),
-                Bluebird.map(n.tasks, function(task, i){
-                        task._test = test._id;
-                        task.index = i
-
-                        var t = new models.Task(task);
-                        test._tasks.push(t._id);
-
-                        // TODO: in here, get the subject id from the subjects array
-                        // _.filter(subjects, function(n){  return n.name === message.name  })
-                        // then create a message-new for every message in the messages array for the task
-                        // then return the saved task in the then step
-                        Bluebird.map(n.tasks._messages, function(note){
-
-                            return modelSave(t);
-                        })
-                })
-            ]).then(function(array){
-                // console.log('is test still set?', test);
-
-                return modelSave(test);
+// Create tests out of the above objects ==================
+    createTests.then(function(tests){
+        // Here, tests is globally tests.
+        return Promise.map(tests, function(test){
+            // now we are in a single test
+            return createSubjects(test, test.subjects).then(function(subjects){
+                // Now subjects is available down the chain.
+                Promise.all([
+                        createTags(test, test.tags),
+                        createTasks(test, test.tasks, subjects)
+                    ])
             })
-        }).then(function(testArray){
-            // aight, did the tests get made?
-            console.log('testArray length', testArray.length);
-
-
-
-        }).then(function(messagedTest){
-            // okay we've posted messages here.
-            callback(null, 'true');
-        }).catch(function(err){
-            if(err){console.log(err);}
-        });
-    })
+        })
+    }).catch(function(err){
+        if(err){console.log(err);}
+    });
 }
