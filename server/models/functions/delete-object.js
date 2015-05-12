42da7f121b00e31022a753dfae65a582741675c8
pretentiousgit <alex.leitch@gmail.com>
2015-05-12 13:57:31 -0400

    added a route to delete tags or tasks indiscriminate of tag or task status, does not remove messages

--- server/models/functions/delete-object.js
+++ server/models/functions/delete-object.js
@@ -7,36 +7,34 @@ module.exports = function(objectKey, type, next){
 // Module dependencies ==========================
     var Promise = require('bluebird');
     var models  = Promise.promisifyAll(require('../../models'));
+    var fn      = require('../../models/functions')
 
 // delete an object
     // find an object
     // remove it from its test
     // NO LONGER: remove all related messages and tags 
     
+    var Model = (type === 'tag') ? models.Tag : 
+                (type === 'task') ? models.Task : 
+                models.Test;
 
-    async.parallel([
-        function(callback){
-            models.Task.findById(task, function(err, doc){
+    Model.findById(objectKey, function(err, doc){
+        if(err){ console.error(err) }
+
+        models.Test.findOne({'_id': doc._test})
+            .exec(function(err, test){
                 if(err){ console.error(err) }
+                
+                var plural = (type === 'tag') ? test._tags : test._tasks;
+                    plural.remove(doc._id);
 
-                models.Test.findOne({'_id': doc._test})
-                    .exec(function(err, test){
-                        if(err){ console.error(err) }
-
-                        test._tasks.remove(doc._id);
-                        test.save(function(err,data){
-                            if(err){ console.error(err); }
-                        });
-                    });
-            })
-            .remove(function(err){
-                if(err){ console.error(err); }
-                callback(null, 'task');
+                test.save(function(err, doc){
+                    if(err){ console.error(err); }
+                });
             });
-        }
-    ], 
-    function(err, results){
+    })
+    .remove(function(err){
         if(err){ console.error(err); }
-        next(task);
+        next(null, 'task');
     });
 };
