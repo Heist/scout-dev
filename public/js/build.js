9a251358c1c546df5b4da44b055aaf53016a9f32
pretentiousgit <alex.leitch@gmail.com>
2015-05-25 14:29:47 -0400

    hashtags?

--- public/js/build.js
+++ public/js/build.js
@@ -50499,6 +50499,18 @@ angular.module('siyfion.sfTypeahead', [])
                                 });
                     }]
                 }
+            })
+
+            // VIDEO STREAM ====================================
+            .state('watch', {
+                url         : '/watch',
+                controller  : 'watch',
+                templateUrl : 'partials/app/watch.html',
+                resolve: { 
+                    loggedin: ['checkLoggedin', function(checkLoggedin) {
+                        return checkLoggedin();
+                    }]
+                }
             });
     });
 
@@ -52392,153 +52404,66 @@ angular.module('siyfion.sfTypeahead', [])
         
     angular.module('field_guide_controls')
     .controller('watch', 
-               ['$scope','$http','$location','$stateParams','$state','$sanitize',
-        function($scope,  $http,  $location,  $stateParams,  $state,  $sanitize){
+               ['$scope','$http','$location','$stateParams','$state','$sanitize','$sce','$window','VideoStream',
+        function($scope,  $http,  $location,  $stateParams,  $state,  $sanitize,  $sce,  $window,  VideoStream){
 
+        // Do we have WebRTC?
+        // function hasGetUserMedia() {
+        //   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        //             navigator.mozGetUserMedia || navigator.msGetUserMedia || window.RTCPeerConnection );
+        // }
 
-        $scope.roomList = [];
+        // if (hasGetUserMedia()) {
+        //   $scope.error = 'WebRTC is not supported by your browser. You can try the app with Chrome and Firefox.';
+        //   return;
+        // }
 
-    // SOCKET ROUTES ================================================
+        var stream;
 
-        // 1.0 -----------
-        // var client = io('http://104.236.16.159:8080/');
+        VideoStream.get()
+            .then(function (s) {
+                stream = s;
+                // Room.init(stream);
+                stream = URL.createObjectURL(stream);
 
-        // 0.9 -----------
-        // TODO ALL OF THIS NEEDS TO BE AN ENVIRONMENT VARIABLE
-        var socket = io.connect('//104.236.16.159:8080/?test='+$stateParams._id, {
-                'force new connection': true
+                $window.stream = stream; // stream available to console
+                var video = document.querySelector("video");
+                video.src = $window.URL.createObjectURL(stream);
+                video.play();
+
+                // if (!$routeParams.roomId) {
+                //     Room.createRoom().then(function (roomId) {
+                //         $location.path('/room/' + roomId);
+                //     });
+                // } else {
+                //     Room.joinRoom($routeParams.roomId);
+                // }
+            }, function () {
+                $scope.error = 'No audio/video permissions. Please refresh your browser and allow the audio/video capturing.';
             });
-        
-        socket.on('message',function(data) {
-            image.src = "data:image/jpg;base64,"+data;
-            canvas.width = 358;
-            canvas.height = 358 * image.height / image.width;
 
-            context.drawImage(image, 0, 0, 358, 358 * image.height / image.width);
-        });
+            // $scope.peers = [];
+    
+            // Room.on('peer.stream', function (peer) {
+            //     console.log('Client connected, adding new stream');
+            //         $scope.peers.push({
+            //         id: peer.id,
+            //         stream: URL.createObjectURL(peer.stream)
+            //     });
+            // });
+    
+            // Room.on('peer.disconnected', function (peer) {
+            //     console.log('Client disconnected, removing stream');
 
-        socket.on('connect_failed', function(data)
-        {
-            
-        });
-        socket.on('connecting', function(data)
-        {
-            
-        });
-        socket.on('disconnect', function(data)
-        {
-            
-            image.src = "/layout/assets/avatar-binocs.jpg";
-            canvas.width = 358;
-            canvas.height = 358 * image.height / image.width;
+            //     $scope.peers = $scope.peers.filter(function (p) {
+            //         return p.id !== peer.id;
+            //     });
+            // });
 
-            context.drawImage(image, 0, 0, 358, 358 * image.height / image.width);
-            socket.socket.disconnect();
-        });
-        socket.on('error', function(reason)
-        {
-            
-        });
-        socket.on('reconnect_failed', function(data)
-        {
-            
-        });
-        socket.on('reconnect', function(data)
-        {
-            
-        });
-        socket.on('reconnecting', function(data)
-        {
-            
-        });
-
-        socket.on('announce', function(data){
-            
-        });
-
-        socket.on('note', function(data){
-            
-            $scope.timeline.push(data.note.msg);
-            $scope.$apply();
-        });
-
-        socket.on('subject', function(data){
-            socket.emit('join_subject_test', data);
-        });
-
-       
-
-    // EMIT SCREENCAPS TO THE SOCKET ====================================
-        var canvas = document.getElementById('channel'),
-            image = document.getElementById('ia'),
-            context = canvas.getContext('2d');
-
-        $scope.connect = {};
-        $scope.connect.text = '71b';
-
-        $scope.subscription = function(chan){
-            
-            socket.emit('subscribe', { room: chan });
-            socket.emit('channel', { room: chan });
+        $scope.getLocalVideo = function () {
+          return $sce.trustAsResourceUrl(stream);
         };
 
-    // ANGULAR ROUTES ===================================================
-        $scope.testName = $stateParams._id; 
-
-        $scope.joinRoom = function(room){
-            
-            // socket.emit('join_room', $scope.selectedRoom.room);
-            $scope.live = true;
-        };
-
-
-        $scope.postMessage = function(message){
-            // here we create a note object
-            var note = {};
-
-            note.body = message;
-            note.tags = [];
-            note.created = new Date();
-             
-            note._task = $scope.selected._id;
-            note._test = $scope.selected._test;
-            // note._session = $stateParams._id;
-            note._subject = $scope.subject._id;
-
-            $scope.timeline.push(note);
-            // 
-
-            // TODO: this will catch things on both sides of the hash. 
-            // if message has # with no space, post that to message.tags
-
-            var hashCatch = new RegExp(/\S*#\S+/gi);
-            var hashPull = new RegExp(/#/gi);
-            var tagIt = message.match(hashCatch);          
-            
-            if (tagIt){
-                for (var i=0; i < tagIt.length; ++i) {
-                    var msg = tagIt[i].replace(hashPull,'');
-                    // // console.log('tag being pushed', msg)
-                    note.tags.push(msg);
-                }
-            }
-            
-            // 
-
-            var url = '/api/message/';
-            var data_out = note;
-
-            $http
-                .post(url, data_out)
-                .success(function(data){
-                    // socket.emit('send:note', { note: data });
-                })
-                .error(function(data){
-                    // 
-                });
-
-            $scope.message='';
-        };
     }]);
 })();
 
@@ -53870,6 +53795,42 @@ angular.module("typeahead-popup.html", []).run(["$templateCache", function($temp
                     }
                 };
             }]);
+})();
+// fg-videostream.js
+// add comments to mesages 
+'use strict';
+
+(function(){
+
+angular.module('field_guide_controls')
+.factory('VideoStream', function ($q) {
+    var stream;
+    return {
+      get: function () {
+        if (stream) {
+          return $q.when(stream);
+        } else {
+          navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+          var constraints = {video: true, audio: true};
+          var d = $q.defer();
+
+          navigator.getUserMedia({
+            video: true,
+            audio: true
+          }, function (s) {
+            stream = s;
+            d.resolve(stream);
+
+
+          }, function (e) {
+            d.reject(e);
+            console.log("navigator.getUserMedia error: ", e);
+          });
+          return d.promise;
+        }
+      }
+    };
+  });
 })();
 // ngMatch.js
 'use strict';
