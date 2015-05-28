0bf4313cbdc4a8f92ee49311b28b4a7d795af595
pretentiousgit <alex.leitch@gmail.com>
2015-05-28 13:37:53 -0400

    added showdown

---
+++ public/js/directives/fg-showdown.js
@@ -0,0 +1,17 @@
+// fg-showdown.js
+// a Showdown converter to permit Markdown in key text fields
+'use strict';
+
+(function(){
+    angular.module('field_guide_controls')
+    .directive('markdown', function() {
+        var converter = new Showdown.converter();
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+                var htmlText = converter.makeHtml(element.text());
+                element.html(htmlText);
+            }
+        }
+    });
+})();
